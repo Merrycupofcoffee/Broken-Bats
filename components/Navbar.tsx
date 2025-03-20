18feb3f68c851ef3c3848e3c3c037f17390c3604
kpoh0003 <kpoh0003@student.monash.edu>
2025-03-20 22:41:26 +1100

    adding link header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -7,6 +8,15 @@ const Navbar = () => {
         <Link href="/">
             <Image src="/BB logo.png" alt = "logo" width={125} height= {125} />
         </Link>
+        
+        <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link) => (
+                <li key={link.href}>
+                    <Link href={link.href} className = "regular-16 text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover: font-bold">{link.label}</Link>
+                </li>
+            ))}
+
+        </ul>
     </nav>
   )
 }
